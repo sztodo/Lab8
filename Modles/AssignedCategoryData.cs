e003e2f54640cc78d78cdf09fbc0971084b5236f
sztodo <57600228+sztodo@users.noreply.github.com>
2021-12-02 14:52:00 +0200

    lab9.1

---
+++ Modles/AssignedCategoryData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sztodolnik_Mihaela_Lab8.Modles
+{
+    public class AssignedCategoryData
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
